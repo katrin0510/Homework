e25128230313c2f7896831b4c3ffe41a29958577
Kate <katrin05.10.91@yande.ru>
2023-01-09 23:18:24 +0400

    Семинар

---
+++ Семинар/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+{
+Console.WriteLine("x = от 1 до + бесконечности");
+Console.WriteLine("y = от 1 до + бесконечности");  
+}
+if (num == 2)
+{
+Console.WriteLine("x = от -1 до - бесконечности");
+Console.WriteLine("y = от 1 до + бесконечности");
+}
+if (num == 3)
+{
+Console.WriteLine("x = от -1 до - бесконечности");
+Console.WriteLine("y = от -1 до - бесконечности");
+}
+if (num == 4)
+{
+Console.WriteLine("x = от 1 до + бесконечности");
+Console.WriteLine("y = от -1 до - бесконечности");
+}
+if (num < 1)
+{
+Console.WriteLine( "введите значение от 1 до 4");
+}
+
+if (num > 4)
+{
+Console.WriteLine( "введите значение от 1 до 4");
+}
+
