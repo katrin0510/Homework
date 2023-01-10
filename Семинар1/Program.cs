efeb5fac569b76cb9b7e513f281ab5987b0bf692
Kate <katrin05.10.91@yande.ru>
2023-01-10 23:02:26 +0400

    Семинар1

---
+++ Семинар1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+// for (int index = 1; index <= num; index++)
+// {
+//     result = result + index;
+// }
+// Console.WriteLine(result);
+
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// while (num > 0)
+// {
+//     num = num / 10;
+//     count++;
+// }
+// Console.WriteLine($"Количество знаков в числе = {count}");
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
