40ebc0a65da7856423d2e782726d7cf5e0e0aaf2
Kate <katrin05.10.91@yande.ru>
2023-01-11 16:22:19 +0400

    Les3Ex23

---
+++ Les3Ex23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+for (int index = 0; index < num; index++)
+{
+array[index] = (index + 1) * (index + 1) * (index +1);
+Console.Write(array[index]);
+}
+
+
