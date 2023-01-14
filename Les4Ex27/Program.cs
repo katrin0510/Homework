cadba9d342564a7492d583069aee1d31c5b179d0
Kate <katrin05.10.91@yande.ru>
2023-01-14 14:47:48 +0400

    Les4Ex27

---
+++ Les4Ex27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+while (num > 0)
+{
+    int x = num % 10;
+    res = res + x;
+    num = num / 10; 
+}
+Console.WriteLine(res);
