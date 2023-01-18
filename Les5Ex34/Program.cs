e9178d8ce5114b0eaf4a457b295abe9963270569
Kate <katrin05.10.91@yande.ru>
2023-01-18 21:50:05 +0400

    Les5Ex34

---
+++ Les5Ex34/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write($"{array[i]} ");
+    if (array[i] % 2 == 0)
+        count++;
+}
+Console.WriteLine();
+Console.Write($"Количество четных элементов: {count}");
+
+
