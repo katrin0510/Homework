589ad7b9e3df04aec1e01694c7bb8ca89ecfdbff
Kate <katrin05.10.91@yande.ru>
2023-01-19 13:39:30 +0400

    Les6Ex41

---
+++ Les6Ex41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество (M) чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write($"{array[i]} ");
+}
+for (int i = 0; i < array.Length; i++)
+{
+   if (array[i] > 0)
+   count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше нуля = {count}");
