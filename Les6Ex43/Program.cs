64a47aa6518afe18ec0a81ef69d7f6192884d5ca
Kate <katrin05.10.91@yande.ru>
2023-01-19 17:27:40 +0400

    Les6Ex43

--- Les6Ex43/Program.cs
+++ Les6Ex43/Program.cs
@@ -4,20 +4,20 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 Console.WriteLine("Введите число b1: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число b2: ");
-double b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число k1: ");
-double k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число k2: ");
-double k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
 if(k1 == k2 && b1 == b2) Console.WriteLine($"Прямые совпадают");
 else if (k1 == k2) Console.WriteLine($"Прямые не пересекаются");
 else
 {
 double x = (b2 - b1) / (k1 - k2);
 double y = k2 * x + b2;
-Console.WriteLine($"({x},{y})");
+Console.WriteLine($"({x}; {y})");
 }
 
 
