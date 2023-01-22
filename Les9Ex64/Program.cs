9d6838975989f2246db41eab74ade6c3219f0535
Kate <katrin05.10.91@yande.ru>
2023-01-22 20:29:41 +0400

    Les9Ex64

---
+++ Les9Ex64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Nat(int N)
+{
+    if (N == 1)
+    {
+        return 1;
+    }
+    Console.Write(N);
+    return Nat(N - 1);
+}
+int x = Nat(N);
+Console.WriteLine(x);
